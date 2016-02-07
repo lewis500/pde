9b9e2b009bf32df7209843a30f21c0112bf7042a
Lewis <lewis500@gmail.com>
2016-02-06 20:18:20 -0800

    axes. about to add road.

--- app/components/plot/plot2.js
+++ app/components/plot/plot2.js
@@ -16,13 +16,11 @@ const {
 const Street = React.createClass({
 	render() {
 		let {
-			time, xScale, yScale, cells, ctx
+			time, xScale, yScale, cells, ctx, w, h
 		} = this.props,
-			x = xScale(time),
-			w = 6,
-			h = 1;
+			y = yScale(time);
 		_.forEach(cells, (d) => {
-			let y = yScale(d.x);
+			let x = xScale(d.x);
 			ctx.fillStyle = d.fill;
 			ctx.fillRect(x, y, w, h);
 		});
@@ -46,61 +44,89 @@ const PlotComponent = React.createClass({
 		right: 5,
 		bottom: 50
 	},
-	_xScale(v) {
-		return this.state.width * (v - this.props.time_range[0]) / (this.props.time_range[1] - this.props.time_range[0]);
+	componentDidMount(){
+		let xAxis = d3.svg.axis()
+			.scale(d3.scale.linear().domain([0,100]).range([0,this.state.width]))
+			.tickSize(-this.state.height);
+		d3.select(this.refs.xAxis)
+			.call(xAxis);
+		let yAxis = d3.svg.axis()
+			.scale(d3.scale.linear().domain([0,NUM_CELLS]).range([this.state.height,0]))
+			.tickSize(-this.state.width)
+			.orient('left');
+		d3.select(this.refs.yAxis)
+			.call(yAxis);
 	},
 	_yScale(v) {
-		return this.state.height * v / NUM_CELLS; //LATER CHANGE THIS TO NUM_CELLS;
+		return this.state.height *(1 - (v - this.props.time_range[0]) / (this.props.time_range[1] - this.props.time_range[0]));
+	},
+	_xScale(v) {
+		return this.state.width * v / NUM_CELLS; //LATER CHANGE THIS TO NUM_CELLS;
 	},
 	_renderCanvas() {
 		let style = {
 			top: this.mar.top,
-			left: this.mar.left
+			left: this.mar.left,
+			// opacity: 
 		};
 		return (
 			<Canvas
-	      width={this.state.width}
-	      height={this.state.height}
-	      style={style}>
+				width={this.state.width}
+				height={this.state.height}
+				style={style}>
 				{
-      _(this.props.history)
-        .filter((d, i) => i <= this.props.time)
-        .map((d, i) => {
-          return StreetFactory({
-            cells: d,
-            yScale: this._yScale,
-            xScale: this._xScale,
-            time: i,
-            key: i
-          });
-        }).value()
-      }
+				_(this.props.history)
+					.filter((d, i) => i <= this.props.time)
+					.map((d, i) => {
+						return StreetFactory({
+							cells: d,
+							yScale: this._yScale,
+							xScale: this._xScale,
+							time: i,
+							key: i,
+							w: this.state.width/NUM_CELLS*.98,
+							h: this.state.height/100*.96
+						});
+					}).value()
+			}
 			</Canvas>
 		);
 	},
 	render() {
 		let {
 			width, height
-		} = this.state;
-		let total_height = this.state.height + this.mar.top + this.mar.bottom,
-			total_width = this.state.width + this.mar.left + this.mar.right;
-		let style = {
-			width: total_width,
-			height: total_height
-		};
+		} = this.state,
+			{
+				left, right, top, bottom
+			} = this.mar,
+			total_height = height + top + bottom,
+			total_width = width + left + right,
+			style = {
+				width: total_width,
+				height: total_height
+			};
 		return (
 			<div 
-      	id='plot'
-      	style={style}>
+				id='plot'
+				style={style}>
 				<svg 
 					width={total_width} 
 					height={total_height}>
 					<g 
-						transform={`translate(${this.mar.left},${this.mar.top})`}>
+						transform={`translate(${left},${top})`}>
 						<rect 
-							width={this.state.width} 
-							height={this.state.height} 
+							width={width} 
+							height={height} 
 							className='background' />
+						<g 
+							ref='xAxis' 
+							className='x axis'
+							transform={`translate(0,${height})`}>
+						</g>
+						<g 
+							ref='yAxis' 
+							className='y axis'>
+						</g>
 					</g>
 				</svg>
 				{this._renderCanvas()}
